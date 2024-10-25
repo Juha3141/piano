ec9719e07f18ab2b13ce70a52d69e2e7612ed88e
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-10-25 16:51:22 +0900

    Update hand_communication_system.hpp

--- include/hand_communication_system.hpp
+++ include/hand_communication_system.hpp
@@ -1,13 +1,23 @@
 #ifndef _HAND_COMMUNICATION_SYSTEM_HPP_
 #define _HAND_COMMUNICATION_SYSTEM_HPP_
 
-struct HandInformation {
-	bool left_motor_on, right_motor_on;
-	int left_finger_number, right_finger_number;
-};
+#include <iostream>
+#include <cstring>
+#include <errno.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
 
-struct CentralInformation {
+typedef struct data_sending_from_center_s {
+	bool motor_on;
+	int finger_number;
+}data_sending_from_center_t;
+
+typedef struct data_received_from_hand_s {
 	bool left[5], right[5];
-};
+}data_received_from_hand_t;
+
+int open_serial_communication(const char *serial_port);
+bool configure_serial_communication(int fd , int speed);
 
 #endif
